a96bc3ca6253f8f679602c8246b1e1bd5804a0a7
george-simon <george-simon@users.noreply.github.com>
2020-10-03 14:36:19 -0700

    revert to first commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,45 +11,34 @@ const runEnter = submit.on("click", function() {
 
     // keep the page from refreshing
     d3.event.preventDefault();
-   
-    // clear the existing output
-    output.clear(cell.text.value);
 
     // select input and get results
     var inputElement = d3.select("#datetime")
 
     // get the value of the input
     var inputValue = inputElement.property("value");
-    
+
     console.log(inputValue)
-    
-    // // use the inputValue to filter on the data set
+
+    // use the inputValue to filter on the data set
     var filtereddata = tableData.filter(date => date.datetime === inputValue);
-    
+
     console.log(filtereddata);
-    // console.log(filtereddata[0].datetime);
 
-    const handleChange = (event) => {
-        // grab the value of the input field
-        let value = d3.event.inputvalue;
-      
-        // clear the existing output
-        output.clear(cell.text);
 
-        filtereddata.forEach(function(UFOdata) { // loop through data and console log each object
-            // console.log(UFOdata);
-            var row = tbody.append("tr") // add tr table elements to html
+    filtereddata.forEach(function(UFOdata) { // loop through data and console log each object
+        // console.log(UFOdata);
+        var row = tbody.append("tr") // add tr table elements to html
 
-            Object.entries(UFOdata).forEach(function([key, value]){ // loop through and console log key value pars
-                // console.log(key, value);
+        Object.entries(UFOdata).forEach(function([key, value]){ // loop through and console log key value pars
+            // console.log(key, value);
 
-                var cell = tbody.append("td"); // add td table elements to html
-                cell.text(value); // attach value to td table elements
-            });
+            var cell = tbody.append("td"); // add td table elements to html
+            cell.text(value); // attach value to td table elements
         });
-    }
+    });
 
-    runEnter.on("change", handleChange);
+});
 
     // if (filtereddata[0].datetime === inputValue) { 
     //     // var filtereddata = tableData.filter(date => date.datetime === inputValue);
@@ -80,22 +69,7 @@ const runEnter = submit.on("click", function() {
     //     });
     // }
 
-    
 
-});
-
-// display ALL the data
-tableData.forEach(function(UFOdata) { // loop through data and console log each object
-    // console.log(UFOdata);
-    var row = tbody.append("tr") // add tr table elements to html
-
-    Object.entries(UFOdata).forEach(function([key, value]){ // loop through and console log key value pars
-        // console.log(key, value);
-
-        var cell = tbody.append("td"); // add td table elements to html
-        cell.text(value); // attach value to td table elements
-    });
-});
 
 // // detecting enter key for input submission
 // // https://stackoverflow.com/questions/7060750/detect-the-enter-key-in-a-text-input-field
