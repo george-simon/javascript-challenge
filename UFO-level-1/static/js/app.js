9190e6b96368dc37fc990075ab91e646fc7aad00
george-simon <george-simon@users.noreply.github.com>
2020-10-03 15:44:01 -0700

    js code updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,17 +2,26 @@
 var tableData = data,
     submit = d3.select("#filter-btn"), // select the submit button
     tbody = d3.select("tbody"),
-    output = d3.select(".output");
+    form = d3.select("#form");
 
+// loop through provide data to show UFO siting data
+tableData.forEach(function(data) { // loop through data and console log each object
+    // console.log(UFOdata);
+    var row = tbody.append("tr") // add tr table elements to html
+
+    Object.entries(data).forEach(function([key, value]){ // loop through and console log key value pars
+        // console.log(key, value);
+
+        var cell = tbody.append("td"); // add td table elements to html
+        cell.text(value); // attach value to td table elements
+    });
+});
 
 // click handler
-const runEnter = submit.on("click", function() {
+const runEnter = () => {
 
     // keep the page from refreshing
     d3.event.preventDefault();
-
-    // clear the existing output
-    output.html("");
 
     // select input and get results
     var inputElement = d3.select("#datetime")
@@ -27,6 +36,10 @@ const runEnter = submit.on("click", function() {
 
     console.log(filtereddata);
 
+    // removes the rows previously inputed or at least I thought it would
+    // tbody.selectAll("tr").remove();
+    d3.select('tableData').remove();
+
     filtereddata.forEach(function(UFOdata) { // loop through data and console log each object
         // console.log(UFOdata);
         var row = tbody.append("tr") // add tr table elements to html
@@ -39,44 +52,7 @@ const runEnter = submit.on("click", function() {
         });
     });
 
-});
+};
 
-    // if (filtereddata[0].datetime === inputValue) { 
-    //     // var filtereddata = tableData.filter(date => date.datetime === inputValue);
-    //     filtereddata.forEach(function(UFOdata) { // loop through data and console log each object
-    //         // console.log(UFOdata);
-    //         var row = tbody.append("tr") // add tr table elements to html
-    
-    //         Object.entries(UFOdata).forEach(function([key, value]){ // loop through and console log key value pars
-    //             // console.log(key, value);
-    
-    //             var cell = tbody.append("td"); // add td table elements to html
-    //             cell.text(value); // attach value to td table elements
-    //         });
-    //     });
-    // }
-    // else {
-    //     // display ALL the data
-    //     tableData.forEach(function(UFOdata) { // loop through data and console log each object
-    //         // console.log(UFOdata);
-    //         var row = tbody.append("tr") // add tr table elements to html
-        
-    //         Object.entries(UFOdata).forEach(function([key, value]){ // loop through and console log key value pars
-    //             // console.log(key, value);
-        
-    //             var cell = tbody.append("td"); // add td table elements to html
-    //             cell.text(value); // attach value to td table elements
-    //         });
-    //     });
-    // }
-
-
-
-// // detecting enter key for input submission
-// // https://stackoverflow.com/questions/7060750/detect-the-enter-key-in-a-text-input-field
-// const node = document.getElementsByClassName("input")[0];
-// node.addEventListener("keyup", function(event) {
-//     if (event.key === "Enter") {
-//         runEnter
-//     }
-// });
+form.on("submit", runEnter);
+submit.on("click", runEnter);
